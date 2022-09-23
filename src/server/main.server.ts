8f6413d3a659b93926180db8ec18542909c4e5a6
nomifluid <nomifluid@gmail.com>
2022-09-23 19:48:00 +0200

    first commit

---
+++ src/server/main.server.ts
@@ -0,0 +1,11 @@
+import { makeHello } from "shared/module";
+
+print(makeHello("main.server.ts"));
+
+const spring = game.Workspace.FindFirstChild("Spring");
+const T = 1;
+
+while (wait(T)) {
+	const t = DateTime.now();
+	print(t);
+}
